a733b54036e9f6ecb07bd1e7f914b0b6a850ca88
Nathan Gould <nathanygould@gmail.com>
2019-03-28 17:01:26 -0400

    Normalized Names, Started Willis-Composite excel

--- TableTest.py
+++ TableTest.py
@@ -20,6 +20,10 @@ for i, df in enumerate(trankTable):
 kenpomDF = pd.read_csv('./CSVFiles/kenpomtable.csv', index_col=0)
 trankDF = pd.read_csv('./CSVFiles/tranktable.csv')
 
+#Correcting for team name difference manually
+trankDF.at[130, 'Team'] = "Charleston"
+kenpomDF.at[199, 'Team'] =  "Fort Wayne"
+#Dropping blank rows
 kenpomDF = kenpomDF.drop([40,41,82,83,124,125,166,167,208,209,250,251,292,293,334,335])
 trankDF = trankDF.drop([25,51,77,103,129,155,181,207,233,259,285,311,337,366])
 trankDF = trankDF.drop('Rec', axis=1)
@@ -38,8 +42,12 @@ rawtrank = trankDF.to_numpy()
 
 #Printing specific data
 #Note some columns of data cannot be printed due to encoding error
-x = 44
+x = 52
 print(rawKenPom[x,1])
 print(rawtrank[x,2])
+
+willisComposite = [['Team', 'Seed', 'Region', 'Net', 'KenPom', 'AdjO', 'AdjD', 'Torvik', 'AEMLast10', 'Q1 W', 'Q1 L', 'Willis Composite']]
+
+print(willisComposite)
 
 input()
